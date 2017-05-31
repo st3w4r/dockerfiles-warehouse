eef35a2e6d4961ef298c2ec6c7bed43ba917fcf3
Yanael Barbier <st3w4r@gmail.com>
2017-05-31 11:54:15 +0200

    Add gdb to dev-c

--- dev-c/run.sh
+++ dev-c/run.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --name dev-c -v $PWD:/app -w /app dev-c /bin/bash
+docker run -it --rm --name dev-c --cap-add=SYS_PTRACE -v $PWD:/app -w /app st3w4r/dockerfiles-warehouse:dev-c /bin/bash
